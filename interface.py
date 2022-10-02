d4c8554f5370e9578aa5807c7aba77c4e1429a33
Julio Macedo <juliomacedochaves@gmail.com>
2022-10-02 20:44:58 -0300

    Showing the informed number of cash machines and the clients in the window

--- interface.py
+++ interface.py
@@ -1,14 +1,89 @@
-from asyncio.windows_events import NULL
-from os import popen
 from tkinter import *
 import tkinter
 from tokenize import Double
 from PIL import Image,ImageTk
-from fluxograma import *
-clients_count = 0
+from threading import Thread
+from multiprocessing import Semaphore
+import time
+
+CLIENTS_COUNT = 0
 ID = 0
+id = 0
 SENHA = 0
 TEMPO = 0
+CAIXAS_COUNT = 0
+s_caixas = Semaphore(0)
+s_clientes = Semaphore()
+s_mutex = Semaphore(1)
+l_caixas = []
+TAC = 0
+
+
+class Cliente(Thread):
+    def __init__(self, id, ta, senha):
+        Thread.__init__(self)
+        self.id = id
+        self.ta = ta
+        self.senha = senha
+
+    def run(self):
+        global TAC, canvas, clientImage
+        print(self.id)
+        s_clientes.acquire()
+        
+        s_mutex.acquire()
+        TAC = int(self.ta)
+        for caixa in l_caixas:
+            if caixa.disponivel == TRUE:
+                
+                caixa_cliente = caixa
+                
+                break
+        drawClient = canvas.create_image(0, 500, anchor=NW, image=clientImage)
+        s_caixas.release()
+        
+
+        print(f"{self.id} está sendo atendido por {caixa_cliente.id}\n \n")
+        
+        t_end = time.time() + int(self.ta)
+        while time.time() < t_end:
+            a=1
+
+
+
+        print(f"Fim do atendimento de {self.id}")
+        print("=========================================")
+        
+
+class Caixa(Thread):
+    def __init__(self, id):
+        Thread.__init__(self)
+        self.id = id
+        self.disponivel = TRUE
+        self.ta = 0
+
+    def run(self):
+            global TAC
+            while True:
+                s_caixas.acquire()             
+                t_end = time.time() + TAC
+                s_mutex.release()
+                print(f"O caixa {self.id} está atendendo um cliente \n \n")
+                self.disponivel = FALSE
+                
+                
+                while time.time() < t_end:
+                    a=1
+
+
+                print(f"O caixa {self.id} está livre \n \n")
+                self.disponivel = TRUE
+
+                s_clientes.release()
+
+        
+
+
 
 def rgb_to_hex(rgb):
     '''
@@ -44,29 +119,35 @@ def click():
     '''
     Função para lidar com o click do start button
     '''
+    global CLIENTS_COUNT, id
     TEMPO = tempo_atendimento_entry.get()
     if TEMPO.strip() == '':
         print('invalido')
     else:
         print(TEMPO)
-    id = clients_count + 1
+    id = id + 1
     ta = TEMPO
-    senha = clients_count + 1
+    senha = CLIENTS_COUNT + 1
     Cliente(id, ta, senha).start()
 
-def open_popup():
-   top= Toplevel(window)
-   top.geometry("500x500")
-   top.title("Quantidade de caixas")
-   t_text = Label(top,text='Insira a quantidade de caixas',font=('Arial',10))
-   t_text.place(x = 200, y = 200)
-   t_entry = Entry(top,width = 5,font=('Arial',20))
-   t_entry.place(x=200,y = 220)
 
-   initial_img = create_button(path_img='assets/start_button.png',scale=0.5)
-   initial_button = Button(top, bd ='2',command=print('henrique bobão'))
-   initial_button.config(image = initial_img)
-   initial_button.place(x = 220,y = 270)
+def click_initial_button():
+    global s_clientes, l_caixas, CAIXAS_COUNT, img, canvas, imgCaixa
+    num_Caixas = t_entry.get()
+    for i in range(int(num_Caixas)):
+        l_caixas.append(Caixa(CAIXAS_COUNT+1))
+        CAIXAS_COUNT+=1
+
+        drawCaixa = canvas.create_image(350 + (i*140), 440, anchor=NW, image=imgCaixa)
+
+
+
+    for caixa in l_caixas:
+        caixa.start()
+    s_clientes = Semaphore(int(num_Caixas))
+
+
+
 # inicializando a janela
 window = Tk()
 # min e max size da janela
@@ -74,19 +155,51 @@ window.minsize(1366,768)
 window.maxsize(1366,768)
 window.configure(bg=rgb_to_hex((204,255,255)))
 
+imgCaixa = create_button("assets/caixa.png", 0.15)
+
 # criando um canvas
 canvas = Canvas(window,width = 1366,height = 600)
 canvas.pack()
 
-open_popup()
+
 
 # setando o back ground
 background = (Image.open('assets/background.png'))
 resized_background = background.resize((1366,600), Image.ANTIALIAS)
 background_image = ImageTk.PhotoImage(resized_background)
 
+
+top= Toplevel(window)
+
+canvasTop = Canvas(top ,width = 500,height = 500)
+canvasTop.pack()
+
+backgroundTop = (Image.open('assets/city.jpg'))
+resized_backgroundTop = backgroundTop.resize((500,500), Image.ANTIALIAS)
+background_imageTop = ImageTk.PhotoImage(resized_backgroundTop)
+
+canvasTop.create_image(0,0,anchor=NW,image = background_imageTop)
+
+top.minsize(500,500)
+top.maxsize(500,500)
+top.title("Quantidade de caixas")
+t_text = Label(top,text='Insira a quantidade de caixas',font=('Cascadia Code SemiBold',10), bg='#696969', fg='#fff')
+t_text.place(x = 150, y = 190)
+t_entry = Entry(top,width = 5,font=('Arial',20))
+t_entry.place(x=210,y = 220)
+
+initial_img = create_button(path_img='assets/start_button.png',scale=0.5)
+initial_button = Button(top, bd ='2',command=click_initial_button)
+initial_button.config(image = initial_img)
+initial_button.place(x = 180,y = 270)
+
+
 # colocando o background na janela
 canvas.create_image(0,0,anchor=NW,image = background_image)
+
+
+clientImage = create_button('assets/client.png', 0.125)
+
 
 # criando os botões de start e exit
 
