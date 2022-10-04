ef96cc8f7d7c83ff59e44f6374e470df4f1316ef
Julio Macedo <juliomacedochaves@gmail.com>
2022-10-04 17:36:15 -0300

    Showing cash machine and client processing

--- interface.py
+++ interface.py
@@ -8,6 +8,7 @@ from multiprocessing import Semaphore
 import time
 import sys
 
+
 CLIENTS_COUNT = 0
 ID = 0
 id = 0
@@ -27,10 +28,14 @@ class Cliente(Thread):
         self.id = id
         self.ta = ta
         self.senha = senha
+        self.count = 0
+        self.image = False
 
     def run(self):
-        global TAC, canvas, clientImage, l_caixas
-        drawClient = canvas.create_image(0, 500, anchor=NW, image=clientImage)
+        global TAC, canvas, clientImage, l_caixas, clientImage2
+        drawClient = canvas.create_image(0, 420, anchor=NW, image=clientImage)
+        label_cliente = Label(window,text=f'{self.id}',font=('Cascadia Code SemiBold',12), bg=rgb_to_hex((204,255,255)))
+        label_cliente.place(x = 0 + 50, y = 395)
         s_clientes.acquire()
         
         s_mutex.acquire()
@@ -44,10 +49,11 @@ class Cliente(Thread):
                 
                 break
 
-
-        while i < 350 + ((int(caixa_cliente.id) - 1) * 140):
+        while i < 200 + ((int(caixa_cliente.id) - 1) * 120):
             canvas.move(drawClient, 10, 0)
+            label_cliente.place(x = i + 50, y = 395)
             i = i + 10
+
         
         s_caixas.release()
         
@@ -55,13 +61,26 @@ class Cliente(Thread):
         print(f"{self.id} está sendo atendido por {caixa_cliente.id}\n \n")
         
         t_end = time.time() + int(self.ta)
+        
         while time.time() < t_end:
-            a=1
+            if self.count == 1000000:
+                if self.image == False:
+                    canvas.delete(drawClient)
+                    drawClient = canvas.create_image(200 + ((int(caixa_cliente.id) - 1) * 120), 420, anchor=NW, image=clientImage2)
+                    self.image = True
+                    self.count = 0
+                else:
+                    canvas.delete(drawClient)
+                    drawClient = canvas.create_image(200 + ((int(caixa_cliente.id) - 1) * 120), 420, anchor=NW, image=clientImage)
+                    self.image = False
+                    self.count = 0
+            self.count += 1
 
         canvas.delete(drawClient)
+        label_cliente.destroy()
 
         print(f"Fim do atendimento de {self.id}\n")
-        print("=========================================\n")
+        print("=================================\n")
         
 
 class Caixa(Thread):
@@ -70,9 +89,10 @@ class Caixa(Thread):
         self.id = id
         self.disponivel = True
         self.ta = 0
+        self.count = 0
 
     def run(self):
-            global TAC
+            global TAC, canvas, caixaImage, caixaImage2
             while True:
                 s_caixas.acquire()             
                 t_end = time.time() + TAC
@@ -81,8 +101,12 @@ class Caixa(Thread):
                 
                 
                 while time.time() < t_end:
-                    a=1
-
+                    if self.count == 15000:
+                        drawCaixa = canvas.create_image(200 + ((int(self.id) - 1) * 120) + 30, 340, anchor=NW, image=caixaImage)
+                        canvas.delete(drawCaixa)
+                        self.count = 0
+                    self.count += 1
+                 
                 self.disponivel = True
                 print(f"O caixa {self.id} está livre \n \n")
                 
@@ -156,7 +180,7 @@ def click_initial_button():
         l_caixas.append(Caixa(CAIXAS_COUNT+1))
         CAIXAS_COUNT+=1
 
-        drawCaixa = canvas.create_image(350 + (i*140), 440, anchor=NW, image=imgCaixa)
+        drawCaixa = canvas.create_image(200 + (i*120), 395, anchor=NW, image=imgCaixa)
 
 
 
@@ -170,21 +194,21 @@ def click_initial_button():
 # inicializando a janela
 window = Tk()
 # min e max size da janela
-window.minsize(1280,720)
-window.maxsize(1280,720)
+window.minsize(800,600)
+window.maxsize(800,600)
 window.configure(bg=rgb_to_hex((204,255,255)))
 
-imgCaixa = create_button("assets/caixa.png", 0.15)
+imgCaixa = create_button("assets/caixa.png", 0.1)
 
 # criando um canvas
-canvas = Canvas(window,width = 1366,height = 600)
+canvas = Canvas(window,width = 800, height = 500)
 canvas.pack()
 
 
 
 # setando o back ground
 background = (Image.open('assets/background.png'))
-resized_background = background.resize((1366,600), Image.ANTIALIAS)
+resized_background = background.resize((800,500), Image.ANTIALIAS)
 background_image = ImageTk.PhotoImage(resized_background)
 
 
@@ -217,31 +241,33 @@ initial_button.place(x = 180,y = 270)
 canvas.create_image(0,0,anchor=NW,image = background_image)
 
 
-clientImage = create_button('assets/client.png', 0.125)
+clientImage = create_button('assets/client.png', 0.1)
+clientImage2 = create_button('assets/charmander2.png', 0.125)
+caixaImage = create_button('assets/cash.png', 0.18)
 
 
 # criando os botões de start e exit
 
-start_img = create_button(path_img='assets/start_button.png',scale=0.7)
+start_img = create_button(path_img='assets/start_button.png',scale=0.5)
 start_button = Button(window, bd ='5',command=click)
 start_button.config(image = start_img)
-start_button.place(x = 50,y = 610)
+start_button.place(x = 20,y = 510)
 
-exit_img = create_button(path_img='assets/exit_button.png',scale=0.7)
+exit_img = create_button(path_img='assets/exit_button.png',scale=0.5)
 exit_button = Button(window,bd = '5', command=window.destroy)
 exit_button.config(image = exit_img )
-exit_button.place(x = 520,y= 610)
+exit_button.place(x = 370,y= 510)
 
 # criando as entry
 
 tempo_atendimento_text = Label(window,text='Insira o tempo de atendimento',font=('Arial',10), bg=rgb_to_hex((204,255,255)))
-tempo_atendimento_text.place(x = 310, y = 630)
+tempo_atendimento_text.place(x = 180, y = 510)
 tempo_atendimento_entry = Entry(window,width = 5,font=('Arial',20))
-tempo_atendimento_entry.place(x=310,y = 650)
+tempo_atendimento_entry.place(x=225,y = 530)
 
-log_widget = ScrolledText(window, height=9, width=70, font=("consolas", "8", "normal"))
+log_widget = ScrolledText(window, height=8, width=40, font=("consolas", "8", "normal"))
 log_widget.pack()
-log_widget.place(x = 800, y =600)
+log_widget.place(x = 520, y =500)
 
 logger = PrintLogger(log_widget)
 sys.stdout = logger
